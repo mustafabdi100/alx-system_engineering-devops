ba0ffee2a1774827bc176e49bd843cf4af915541
mustafabdi100 <mustafa300abdi@gmail.com>
2023-02-08 14:04:00 +0300

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,12 @@
 #!/usr/bin/python3
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        user_id = int(sys.argv[1])
-        url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-        response = requests.get(url)
-        if response.status_code == 200:
-            user = response.json()
-            employee_name = user.get("name")
-
-            url = "https://jsonplaceholder.typicode.com/todos"
-            response = requests.get(url)
-            if response.status_code == 200:
-                todos = response.json()
-                done_todos = [todo for todo in todos if todo.get("userId") == user_id and todo.get("completed") is True]
-                done_todos_count = len(done_todos)
-                total_todos_count = len(todos)
-
-                print("Employee Name: OK")
-                print("To Do Count: OK")
-                print("First line formatting: OK")
-
-                print("Employee {} is done with tasks({}/{}):".format(employee_name, done_todos_count, total_todos_count))
-                for i, todo in enumerate(done_todos, start=1):
-                    print("Task {} in output: OK".format(i))
-                    print("Task {} Formatting: OK".format(i))
-                    print("\t {}".format(todo.get("title")))
-            else:
-                print("Error: status code {}".format(response.status_code))
-        else:
-            print("Error: status code {}".format(response.status_code))
-    else:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    finished = [t.get('title') for t in todos if t.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(finished), len(todos)))
+    [print("\t {}".format(c)) for c in finished]
