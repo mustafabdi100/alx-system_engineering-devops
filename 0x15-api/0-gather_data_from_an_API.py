5df9d8ef13c8fa582325336e4993d255909c8b51
mustafabdi100 <mustafa300abdi@gmail.com>
2023-02-08 13:45:27 +0300

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,26 +8,35 @@ import re
 import requests
 import sys
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        exit(1)
 
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
+    employee_id = sys.argv[1]
+    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
+    # Get employee name
+    response = requests.get(api_url)
+    if response.status_code != 200:
+        print("Error: Could not retrieve employee information")
+        exit(1)
+    employee = response.json()
+    employee_name = employee["name"]
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    # Get employee todo list
+    response = requests.get(todo_url)
+    if response.status_code != 200:
+        print("Error: Could not retrieve todo list")
+        exit(1)
+    todos = response.json()
+    completed_todos = [todo["title"] for todo in todos if todo["completed"]]
+    total_todos = len(todos)
+    done_todos = len(completed_todos)
+
+    # Print employee todo list progress
+    print(f"Employee {employee_name} is done with tasks({done_todos}/{total_todos}):")
+    for todo in completed_todos:
+        print(f"\t {todo}")
+
