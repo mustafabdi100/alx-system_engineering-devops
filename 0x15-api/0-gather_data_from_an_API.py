5f44bd3ca5c5f03f6ab98b1462d110591a2a044a
mustafabdi100 <mustafa300abdi@gmail.com>
2023-02-08 13:49:56 +0300

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,34 +9,26 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        exit(1)
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        user_id = int(sys.argv[1])
+        url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+        response = requests.get(url)
+        if response.status_code == 200:
+            user = response.json()
+            employee_name = user.get("name")
 
-    employee_id = sys.argv[1]
-    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+            url = "https://jsonplaceholder.typicode.com/todos"
+            response = requests.get(url)
+            if response.status_code == 200:
+                todos = response.json()
+                done_todos = [todo for todo in todos if todo.get("userId") == user_id and todo.get("completed") is True]
+                done_todos_count = len(done_todos)
+                total_todos_count = len(todos)
 
-    # Get employee name
-    response = requests.get(api_url)
-    if response.status_code != 200:
-        print("Error: Could not retrieve employee information")
-        exit(1)
-    employee = response.json()
-    employee_name = employee["name"]
-
-    # Get employee todo list
-    response = requests.get(todo_url)
-    if response.status_code != 200:
-        print("Error: Could not retrieve todo list")
-        exit(1)
-    todos = response.json()
-    completed_todos = [todo["title"] for todo in todos if todo["completed"]]
-    total_todos = len(todos)
-    done_todos = len(completed_todos)
-
-    # Print employee todo list progress
-    print(f"Employee {employee_name} is done with tasks({done_todos}/{total_todos}):")
-    for todo in completed_todos:
-        print(f"\t {todo}")
-
+                print("Employee {} is done with tasks({}/{}):".format(employee_name, done_todos_count, total_todos_count))
+                for todo in done_todos:
+                    print("\t {}".format(todo.get("title")))
+        else:
+            print("Error: status code {}".format(response.status_code))
+    else:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
